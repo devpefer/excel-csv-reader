f5e22f143c9cb050038ecc26394633953d5ea71e
devpefer <devpefer@gmail.com>
2026-04-03 17:57:58 +0200

    csv reader fully implemented

--- ImportadorExcelCsv.Application/CsvItemReader.cs
+++ ImportadorExcelCsv.Application/CsvItemReader.cs
@@ -1,5 +1,9 @@
-﻿using ImportadorExcelCsv.Domain.Enums;
+﻿using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using ImportadorExcelCsv.Domain.Enums;
 using ImportadorExcelCsv.Domain.Interfaces;
+using ImportadorExcelCsv.Domain.ValueObjects;
 using ImportadorExcelCsv.Items;
 
 namespace ImportadorExcelCsv.Application;
@@ -9,20 +13,40 @@ public class CsvItemReader : IItemReader
   public List<Item> Read(string filePath, bool hasHeader)
   {
     List<Item> items = [];
+    var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+    {
+      Delimiter = ",",
+      HasHeaderRecord = hasHeader
+    };
+    
+    using var reader = new StreamReader(filePath);
+    using var csv = new CsvReader(reader, config);
 
+    if (hasHeader)
+    {
+      csv.Read();
+      csv.ReadHeader();
+    }
+    
+    while (csv.Read())
+    {
+      items.Add(GetItem(csv));
+    }
+    
     return items;
   }
 
-  private Item GetItem()
+  private Item GetItem(CsvReader csv)
   {
+    bool validCategory = Enum.TryParse(typeof(Category), csv.GetField("Categoria").Trim(), true, out var category);
     return Item.Create
     (
-      row.Cell(1).GetValue<string>(),
-      row.Cell(2).GetValue<string>(),
-      row.Cell(3).GetValue<decimal>(),
-      row.Cell(4).GetValue<int>(),
-      row.Cell(5).GetValue<Category>(),
-      row.Cell(6).GetValue<bool>()
+      new SKU(csv.GetField("SKU").Trim()),
+      csv.GetField("Nombre").Trim(),
+      decimal.Parse(csv.GetField("Precio"), CultureInfo.InvariantCulture),
+      int.Parse(csv.GetField("Stock"), CultureInfo.InvariantCulture),
+      (Category)category,
+      bool.Parse(csv.GetField("Activo"))
     );
   }
 }
